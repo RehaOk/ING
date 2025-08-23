cdec8c084edf3b8f94c0b6151056c4792aaaf045
RehaOk <rehaok0@gmail.com>
2025-08-23 19:03:46 +0300

    Project Icons Added

---
+++ src/icons/index.js
@@ -0,0 +1,6 @@
+export * from './xIcon.js';
+export * from './trashIcon.js';
+export * from './editPenIcon.js';
+export * from './employeeIcon.js';
+export * from './listIcon.js';
+export * from './gridIcon.js';
