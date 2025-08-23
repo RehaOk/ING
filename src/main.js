9babda45c116a1ed425092dc52b8ee49f79574fa
RehaOk <rehaok0@gmail.com>
2025-08-23 19:06:07 +0300

    Routes using vaadin added, NODE_ENV error fixed

--- src/main.js
+++ src/main.js
@@ -1,2 +1,12 @@
 import {Router} from '@vaadin/router';
-import {store} from './store.js';
+import {store} from './store.js'; // TODO: Remove if won't be needed
+import './routes/home/home.js';
+import './routes/update/update.js';
+
+const outlet = document.getElementById('outlet');
+const router = new Router(outlet);
+
+router.setRoutes([
+  {path: '/', component: 'home-view'},
+  {path: '/update', component: 'update-view'},
+]);
