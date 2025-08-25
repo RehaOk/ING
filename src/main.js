12a1be44e958f3e2a015f690f845782401dae0bf
RehaOk <rehaok0@gmail.com>
2025-08-25 12:52:08 +0300

    Added another page for creating employee

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import {Router} from '@vaadin/router';
-import {store} from './store.js'; // TODO: Remove if won't be needed
 import './routes/home/home.js';
 import './routes/update/update.js';
+import './routes/create/create.js';
 
 const outlet = document.getElementById('outlet');
 const router = new Router(outlet);
@@ -9,4 +9,5 @@ const router = new Router(outlet);
 router.setRoutes([
   {path: '/', component: 'home-view'},
   {path: '/update', component: 'update-view'},
+  {path: '/add', component: 'create-view'},
 ]);
