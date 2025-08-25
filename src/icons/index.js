3b64da52305636396bd4fa0c82b8a08ab8e434dd
RehaOk <rehaok0@gmail.com>
2025-08-25 12:49:36 +0300

    Updated js Icons with SVG files, created global styles file

--- src/icons/index.js
+++
@@ -1,6 +0,0 @@
-export * from './xIcon.js';
-export * from './trashIcon.js';
-export * from './editPenIcon.js';
-export * from './employeeIcon.js';
-export * from './listIcon.js';
-export * from './gridIcon.js';
