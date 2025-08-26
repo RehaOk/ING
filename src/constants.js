e88a3df13c62bf868dbeebbfea2232db968f07f9
RehaOk <rehaok0@gmail.com>
2025-08-27 00:14:14 +0300

    Added Constant Definitions for Pagination Slice

--- src/constants.js
+++ src/constants.js
@@ -7,3 +7,13 @@ export const POSITIONS = {
   MEDIOR: 'Medior',
   SENIOR: 'Senior',
 };
+
+export const INITIAL_PAGINATION_STATE = {
+  CURRENT_PAGE: 1,
+  ITEMS_PER_PAGE: 4,
+  TOTAL_PAGE_NUMBER: 2,
+};
+
+export const GRID_MODE_ITEMS_PER_PAGE = 4;
+export const LIST_MODE_ITEMS_PER_PAGE = 9;
+export const FIRST_PAGE_NUMBER = 1;
--- src/slices/paginationSlice.js
+++ src/slices/paginationSlice.js
@@ -1,12 +1,17 @@
 import {createSlice} from '@reduxjs/toolkit';
-
+import {
+  INITIAL_PAGINATION_STATE,
+  GRID_MODE_ITEMS_PER_PAGE,
+  LIST_MODE_ITEMS_PER_PAGE,
+  FIRST_PAGE_NUMBER,
+} from '../constants.js';
 const paginationSlice = createSlice({
   name: 'pagination',
   initialState: {
-    currentPage: 1,
-    itemsPerPage: 4,
+    currentPage: INITIAL_PAGINATION_STATE.CURRENT_PAGE,
+    itemsPerPage: INITIAL_PAGINATION_STATE.ITEMS_PER_PAGE,
     employeeListToDisplay: [],
-    totalPageNumber: 2,
+    totalPageNumber: INITIAL_PAGINATION_STATE.TOTAL_PAGE_NUMBER,
   },
   reducers: {
     setCurrentPage(state, action) {
@@ -32,11 +37,17 @@ const paginationSlice = createSlice({
       let itemsPerPage, newPageNumber;
 
       if (viewMode === 'grid') {
-        itemsPerPage = 4;
-        newPageNumber = Math.floor((currentPage * 9) / 4) || 1;
+        itemsPerPage = GRID_MODE_ITEMS_PER_PAGE;
+        newPageNumber =
+          Math.floor(
+            (currentPage * LIST_MODE_ITEMS_PER_PAGE) / GRID_MODE_ITEMS_PER_PAGE
+          ) || FIRST_PAGE_NUMBER;
       } else {
-        itemsPerPage = 9;
-        newPageNumber = Math.round((currentPage * 4) / 9) || 1;
+        itemsPerPage = LIST_MODE_ITEMS_PER_PAGE;
+        newPageNumber =
+          Math.round(
+            (currentPage * GRID_MODE_ITEMS_PER_PAGE) / LIST_MODE_ITEMS_PER_PAGE
+          ) || FIRST_PAGE_NUMBER;
       }
 
       const totalPageNumber = Math.ceil(employeeList.length / itemsPerPage);
@@ -45,13 +56,13 @@ const paginationSlice = createSlice({
         newPageNumber = totalPageNumber;
       }
 
-      if (newPageNumber < 1) {
-        newPageNumber = 1;
+      if (newPageNumber < FIRST_PAGE_NUMBER) {
+        newPageNumber = FIRST_PAGE_NUMBER;
       }
 
       state.itemsPerPage = itemsPerPage;
       state.currentPage = newPageNumber;
-      const start = (newPageNumber - 1) * itemsPerPage;
+      const start = (newPageNumber - 1) * itemsPerPage; // -1 for zero based index
       const end = start + itemsPerPage;
       state.employeeListToDisplay = employeeList.slice(start, end);
       state.totalPageNumber = Math.ceil(employeeList.length / itemsPerPage);
